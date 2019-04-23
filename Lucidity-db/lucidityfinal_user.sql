2699c8f29bb38b8d24aa13f8f4dd07a76b7b98c9
Leidy <arqleidyjohana@gmail.com>
2019-04-23 19:59:04 +0100

    First commit

---
+++ Lucidity-db/lucidityfinal_user.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: lucidityfinal
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (16,'leidy johana rios santofimio','santofimio','e9d47b36f65e2a1d9c32bf17e756345e','arqleidyjohana2@gmail.com',43,'Málaga',1),(17,'Pedro','Hernandez','e9d47b36f65e2a1d9c32bf17e756345e','hernandez@gmail.com',65,'Málaga',1),(18,'Johana','Rios','e9d47b36f65e2a1d9c32bf17e756345e','johana@gmail.com',34,'Málaga',1),(19,'Claudio','Perez','e9d47b36f65e2a1d9c32bf17e756345e','ksdkmsd@gmail.com',44,'Málaga',1),(20,'Hunbert','Castro','e9d47b36f65e2a1d9c32bf17e756345e','castrohumbert@gmail.com',56,'Madrid',1),(21,'prueba','pruebaPEPITA','e9d47b36f65e2a1d9c32bf17e756345e','someemail@gmail.com',45,'Torre del Mar por Cali',1),(22,'test','pruebaPEPITA','6a491c0eac66c1fabc45c2557be776b7','testapi@gmail.com',45,'Torre del Mar por Cali',1),(38,'tes9','tes9','e376699d5b6095802b5a27a2a86fcc0a','tes9@gmail.com',15,'Málaga',1),(39,'preReact','pruebaPEPITA','6a491c0eac66c1fabc45c2557be776b7','testapi2@gmail.com',45,'Torre del Mar por Cali',1),(40,'pruebaAPIdesdeReact','santofimio','6a491c0eac66c1fabc45c2557be776b7','arqleidyjohana32@gmail.com',32,'Málaga',1),(41,'Alejandro','Rodriguez','6a491c0eac66c1fabc45c2557be776b7','arodriguezcabanillas@gmail.com',32,'Madrid',1),(42,'TES77','TES77','6a491c0eac66c1fabc45c2557be776b7','TES77@gmail.com',16,'Málaga',1),(43,'test11','test11','6a491c0eac66c1fabc45c2557be776b7','test11@gmail.com',34,'test11',1),(44,'Paco','paco','6a491c0eac66c1fabc45c2557be776b7','paco@gmail.com',255,'Málaga',1),(45,'NIco','Kruk','6a491c0eac66c1fabc45c2557be776b7','kruk@gmail.com',27,'Málaga',1),(46,'Paca','Gallego','6a491c0eac66c1fabc45c2557be776b7','paca@gmail.com',61,'Málaga',1),(47,'pruebamil','pruebamil','6a491c0eac66c1fabc45c2557be776b7','pruebamil@gmail.com',34,'mdpp',1),(48,'test555','test555','6a491c0eac66c1fabc45c2557be776b7','test555@gmail.com',65,'Málaga',1),(49,'test22','test22','6a491c0eac66c1fabc45c2557be776b7','test22@gmail.com',45,'Málaga',1),(50,'Gloria','Patiño','6a491c0eac66c1fabc45c2557be776b7','patiño@gmail.com',37,'Granada',1),(51,'test10','test10','6a491c0eac66c1fabc45c2557be776b7','test10@gmail.com',41,'Málaga',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-23 19:34:00
---
+++ README.md
@@ -0,0 +1,46 @@
+﻿DEPENDENCIES: 
+
+NPM INSTALL =====> After NPM install the following dependecies. 
+The versions are only at an informative level
+
+"axios": "^0.18.0",
+    
+"bootstrap": "^4.3.1",
+    
+"react": "^16.8.6",
+    
+"react-dom": "^16.8.6",
+    
+"react-redux": "^7.0.2",
+    
+"react-router-dom": "^5.0.0",
+    
+"react-scripts": "2.1.8",
+    
+"redux": "^4.0.1",
+    
+"redux-thunk": "^2.3.0",
+    
+"request": "^2.88.0"
+
+“body-parser”: “^1.18.3”,
+“cookie-parser”: “~1.4.3”,
+“crypto”: “^1.0.1”,
+“debug”: “~2.6.9”,
+“ejs”: “~2.5.7”,
+“express”: “~4.16.0”,
+“http-errors”: “~1.6.2”,
+“jsonwebtoken”: “^8.5.0”,
+“morgan”: “~1.9.0”,
+“mysql2”: “^1.6.5”,
+“passport”: “^0.4.0”,
+“uniq”: “^1.0.1”
+
+
+NPM specific =====>
+
+npm install -g node-modules
+npm install -g nodemon
+
+
+    
